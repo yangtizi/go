f8026b1c684ed87c42941ace36b9126335ef670a
warrially <warrially@139.com>
2019-09-08 15:48:11 +0800

    依赖库

--- YLK依赖库.bat
+++ YLK依赖库.bat
@@ -1,8 +1,8 @@
-go get github.com/davecgh/go-spew/spew
-go get github.com/pmezard/go-difflib/difflib
-go get gopkg.in/yaml.v2
-go get github.com/valyala/fasthttp
-go get go.uber.org/zap
-go get go.uber.org/zap/zapcore
-go get gopkg.in/natefinch/lumberjack.v2    
-go get github.com/denisenkom/go-mssqldb
+go get -v github.com/davecgh/go-spew/spew
+go get -v github.com/pmezard/go-difflib/difflib
+go get -v gopkg.in/yaml.v2
+go get -v github.com/valyala/fasthttp
+go get -v go.uber.org/zap
+go get -v go.uber.org/zap/zapcore
+go get -v gopkg.in/natefinch/lumberjack.v2    
+go get -v github.com/denisenkom/go-mssqldb
--- YLK依赖库.sh
+++
@@ -1,8 +0,0 @@
-go get github.com/davecgh/go-spew/spew
-go get github.com/pmezard/go-difflib/difflib
-go get gopkg.in/yaml.v2
-go get github.com/valyala/fasthttp
-go get go.uber.org/zap
-go get go.uber.org/zap/zapcore
-go get gopkg.in/natefinch/lumberjack.v2    
-go get github.com/denisenkom/go-mssqldb
