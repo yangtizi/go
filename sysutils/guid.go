963cb51c8e8f1235ebd918436f964af3d29518d3
warrially <warrially@139.com>
2019-09-17 13:07:54 +0800

    添加guid

---
+++ sysutils/guid.go
@@ -0,0 +1,22 @@
+package sysutils
+
+import (
+	uuid "github.com/satori/go.uuid"
+)
+
+// GenUUID 获取uuid
+func GenUUID() string {
+	guid, _ := uuid.NewV4()
+	strGUID := guid.String()
+	strGUID = StringReplaceAll(strGUID, "-", "")
+	strGUID = UpperCase(strGUID)
+	return strGUID
+}
+
+// GenGUID 生成GUID
+func GenGUID() string {
+	guid, _ := uuid.NewV4()
+	strGUID := guid.String()
+	strGUID = UpperCase(strGUID)
+	return strGUID
+}
