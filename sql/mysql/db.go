0075f99075171c9770ad3ada0525dc76da1ca452
warrially <warrially@139.com>
2019-09-19 13:15:21 +0800

    mysql 支持

---
+++ sql/mysql/db.go
@@ -0,0 +1,57 @@
+package mssql
+
+import (
+	"database/sql"
+	"errors"
+	"sync"
+
+	_ "github.com/go-sql-driver/mysql" // mysql 数据库
+	log "github.com/yangtizi/go/log/zaplog"
+)
+
+var mapMYSQL sync.Map
+
+// QueryRow (strAgent 代理商编号, strQuery sql脚本, args 脚本参数)
+func QueryRow(strAgent string, strQuery string, args ...interface{}) (*sql.Row, error) {
+	v, ok := mapMYSQL.Load(strAgent)
+	if !ok {
+		return nil, errors.New("不存在的DB索引")
+	}
+
+	return v.(*TMySQLDB).queryRow(strQuery, args...)
+}
+
+// QueryRows (strAgent 代理商编号, strQuery sql脚本, args 脚本参数)
+func QueryRows(strAgent string, strQuery string, args ...interface{}) (*sql.Rows, error) {
+	v, ok := mapMYSQL.Load(strAgent)
+	if !ok {
+		return nil, errors.New("不存在的DB索引")
+	}
+
+	return v.(*TMySQLDB).queryRows(strQuery, args...)
+}
+
+// Exec (strAgent 代理商编号, strQuery sql脚本, args 脚本参数)
+func Exec(strAgent string, strQuery string, args ...interface{}) (sql.Result, error) {
+	v, ok := mapMYSQL.Load(strAgent)
+	if !ok {
+		return nil, errors.New("不存在的DB索引")
+	}
+
+	return v.(*TMySQLDB).exec(strQuery, args...)
+}
+
+// InitDB 初始化DB (strAgent 代理商编号, strConnect 从库连接字符串)
+func InitDB(strAgent string, strConnect string) {
+	_, ok := mapMYSQL.Load(strAgent)
+	if !ok {
+		// * 创建新的DB指针
+		pMsSQL := NewDB(strConnect)
+
+		log.Println("正在连接数据库 ", strAgent, strConnect)
+		mapMYSQL.Store(strAgent, pMsSQL)
+		return
+	}
+
+	log.Println("已经存在确有重复创建")
+}
---
+++ sql/mysql/mysqldb.go
@@ -0,0 +1,67 @@
+package mssql
+
+import (
+	"database/sql"
+	"errors"
+
+	log "github.com/yangtizi/go/log/zaplog"
+)
+
+// TMySQLDB 单个的数据库
+type TMySQLDB struct {
+	chpool     chan int
+	strConnect string
+	pDB        *sql.DB
+}
+
+// NewDB 创建新的MSSQL数据库类
+func NewDB(strReadConnect string) *TMySQLDB {
+	p := &TMySQLDB{}
+	p.init(strReadConnect)
+	return p
+}
+
+func (self *TMySQLDB) init(strConnect string) {
+	db, err := sql.Open("mysql", strConnect)
+	if err == nil {
+		self.pDB = db
+		self.strConnect = strConnect
+		self.chpool = make(chan int, 30)
+		return
+	}
+
+	log.Println("数据库连接出现问题 connect = ", strConnect, " err = ", err)
+}
+
+func (self *TMySQLDB) queryRow(strQuery string, args ...interface{}) (*sql.Row, error) {
+	if self.pDB == nil {
+		return nil, errors.New("不存在DB")
+	}
+
+	self.chpool <- 1
+	row := self.pDB.QueryRow(strQuery, args...)
+	<-self.chpool
+	return row, nil
+}
+
+func (self *TMySQLDB) queryRows(strQuery string, args ...interface{}) (*sql.Rows, error) {
+	if self.pDB == nil {
+		return nil, errors.New("不存在DB")
+	}
+	self.chpool <- 1
+	rows, err := self.pDB.Query(strQuery, args...)
+	<-self.chpool
+	return rows, err
+}
+
+func (self *TMySQLDB) exec(strQuery string, args ...interface{}) (sql.Result, error) {
+
+	if self.pDB == nil {
+		return nil, errors.New("不存在DB")
+	}
+
+	self.chpool <- 1
+	rs, err := self.pDB.Exec(strQuery, args...)
+	<-self.chpool
+	return rs, err
+}
