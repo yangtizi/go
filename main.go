de625e11861bc3562c3806c0bbe69e6919a52b14
warrially <warrially@139.com>
2019-09-27 19:19:31 +0800

    增加几个新库 方便使用

--- main.go
+++ main.go
@@ -4,13 +4,17 @@ import (
 	_ "github.com/davecgh/go-spew/spew"       // .
 	_ "github.com/denisenkom/go-mssqldb"      // .
 	_ "github.com/go-sql-driver/mysql"        // .
-	_ "github.com/gomodule/redigo"            // .
+	_ "github.com/golang/protobuf/proto"      // .
+	_ "github.com/gomodule/redigo/redis"      // .
+	_ "github.com/google/uuid"                // .
 	_ "github.com/gorilla/websocket"          // .
 	_ "github.com/kataras/iris"               // .
 	_ "github.com/labstack/gommon/log"        // .
 	_ "github.com/pmezard/go-difflib/difflib" // .
 	_ "github.com/russross/blackfriday"       // .
 	_ "github.com/satori/go.uuid"             // .
+	_ "github.com/thinkoner/openssl"          //.
+	_ "github.com/tuotoo/qrcode"              // .
 	_ "github.com/valyala/fasthttp"           // .
 	_ "github.com/valyala/fasttemplate"       // .
 	_ "go.uber.org/zap"                       // .
