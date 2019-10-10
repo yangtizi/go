87d09d2974acb8030d6898bc31ade078a2e880dc
warrially <warrially@139.com>
2019-10-10 19:10:12 +0800

    加上随机

--- sysutils/guid.go
+++ sysutils/guid.go
@@ -1,10 +1,13 @@
 package sysutils
 
 import (
+	"math/rand"
+	"time"
+
 	uuid "github.com/satori/go.uuid"
 )
 
-// GenUUID 获取uuid
+// GenUUID 获取uuid xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 func GenUUID() string {
 	guid, _ := uuid.NewV4()
 	strGUID := guid.String()
@@ -13,10 +16,33 @@ func GenUUID() string {
 	return strGUID
 }
 
-// GenGUID 生成GUID
+// GenGUID 生成GUID xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
 func GenGUID() string {
 	guid, _ := uuid.NewV4()
 	strGUID := guid.String()
 	strGUID = UpperCase(strGUID)
 	return strGUID
+}
+
+// GenRandomNumber 生成随机的字符串
+func GenRandomNumber(nLen int) string {
+	bytes := []byte("0123456789")
+	result := []byte{}
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for i := 0; i < nLen; i++ {
+		result = append(result, bytes[r.Intn(len(bytes))])
+	}
+	return string(result)
+}
+
+// GenRandomHex 生成随机字符串
+func GenRandomHex(nLen int) string {
+	s := GenUUID()
+	for {
+		if nLen < len(s) {
+			break
+		}
+		s += GenUUID()
+	}
+	return s[0:nLen]
 }
