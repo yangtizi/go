206ddd7900e73a2a0df878a60bfeeab2d519b1c6
warrially <warrially@139.com>
2019-10-10 14:39:40 +0800

    stringbuilder 加大拼接效率

---
+++ sysutils/stringbuilder.go
@@ -0,0 +1,72 @@
+package sysutils
+
+import (
+	"bytes"
+	"strconv"
+	"strings"
+)
+
+// TStringBuilder .
+type TStringBuilder struct {
+	buffer *bytes.Buffer
+}
+
+// NewStringBuilder 新建String
+func NewStringBuilder() *TStringBuilder {
+	p := &TStringBuilder{}
+	p.Clear()
+	return p
+}
+
+// Append 追加
+func (self *TStringBuilder) Append(s string) *TStringBuilder {
+	self.buffer.WriteString(s)
+	return self
+}
+
+//
+// AppendStrings .
+func (self *TStringBuilder) AppendStrings(ss ...string) *TStringBuilder {
+	for i := range ss {
+		self.buffer.WriteString(ss[i])
+	}
+	return self
+}
+
+// AppendInt .
+func (self *TStringBuilder) AppendInt(i int) *TStringBuilder {
+	self.buffer.WriteString(strconv.Itoa(i))
+	return self
+}
+
+// AppendInt64 .
+func (self *TStringBuilder) AppendInt64(i int64) *TStringBuilder {
+	self.buffer.WriteString(strconv.FormatInt(i, 10))
+	return self
+}
+
+// AppendFloat64 .
+func (self *TStringBuilder) AppendFloat64(f float64) *TStringBuilder {
+	self.buffer.WriteString(strconv.FormatFloat(f, 'E', -1, 64))
+	return self
+}
+
+// Replace .
+func (self *TStringBuilder) Replace(old, new string) *TStringBuilder {
+	str := strings.Replace(self.ToString(), old, new, -1)
+	self.Clear()
+	self.buffer.WriteString(str)
+	return self
+}
+
+// Clear .
+func (self *TStringBuilder) Clear() *TStringBuilder {
+	buffer := &bytes.Buffer{}
+	self.buffer = buffer
+	return self
+}
+
+// ToString .
+func (self *TStringBuilder) ToString() string {
+	return self.buffer.String()
+}
