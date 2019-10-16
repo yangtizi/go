63ae007d6a85f01de58c8f6d66e4aea2d500321e
warrially <warrially@139.com>
2019-10-16 17:23:43 +0800

    // BlockCopy 拷贝

--- sysutils/bytes.go
+++ sysutils/bytes.go
@@ -2,6 +2,7 @@ package sysutils
 
 import (
 	"bytes"
+	"errors"
 	"math/rand"
 )
 
@@ -29,4 +30,22 @@ func Concat(a []byte, b []byte) []byte {
 	buff.Write(a)
 	buff.Write(b)
 	return buff.Bytes()
+}
+
+// BlockCopy 拷贝
+func BlockCopy(src []byte, srcOffset int, dst []byte, dstOffset, count int) (bool, error) {
+	srcLen := len(src)
+	if srcOffset > srcLen || count > srcLen || srcOffset+count > srcLen {
+		return false, errors.New("源缓冲区 索引超出范围")
+	}
+	dstLen := len(dst)
+	if dstOffset > dstLen || count > dstLen || dstOffset+count > dstLen {
+		return false, errors.New("目标缓冲区 索引超出范围")
+	}
+	index := 0
+	for i := srcOffset; i < srcOffset+count; i++ {
+		dst[dstOffset+index] = src[srcOffset+index]
+		index++
+	}
+	return true, nil
 }
