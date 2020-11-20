b2a90938426ed5020408ddd79433375d579ae094
warrially <warrially@139.com>
2020-11-20 16:48:24 +0800

    增加几个常用函数

--- sysutils/strings.go
+++ sysutils/strings.go
@@ -93,3 +93,138 @@ func Trim(s string) string {
 func TrimEx(s string, tm string) string {
 	return strings.Trim(s, tm)
 }
+
+// TrimLeft 去掉左边
+func TrimLeft(s string) string {
+	return strings.TrimLeft(s, " ")
+}
+
+// TrimRight 去掉右边
+func TrimRight(s string) string {
+	return strings.TrimRight(s, " ")
+}
+
+// ToLower 转小写
+func ToLower(s string) string {
+	return strings.ToLower(s)
+}
+
+// ToUpper 转大写
+func ToUpper(s string) string {
+	return strings.ToUpper(s)
+}
+
+// Compare 比较 0： a==b,  -1 ： a < b,   +1 ： a > b.
+func Compare(a, b string) int {
+	return strings.Compare(a, b)
+}
+
+// IndexOf 字符串匹配
+// sysutils.IndexOf("slisa", "is") // 2
+func IndexOf(s, sub string) int {
+	return strings.Index(s, sub) // -1; 没找到
+}
+
+// LastIndexOf 字符串匹配
+// sysutils.LastIndexOf("slisa", "is") // 2
+func LastIndexOf(s, sub string) int {
+	return strings.LastIndex(s, sub)
+}
+
+// IndexOfAny 字符串匹配，后面是任意包含
+// sysutils.IndexOfAny("slisa", "is") // 0
+func IndexOfAny(s, chars string) int {
+	return strings.IndexAny(s, chars)
+}
+
+// LastIndexOfAny 字符串匹配，后面是任意包含
+// sysutils.LastIndexOfAny("slisa", "is") // 3
+func LastIndexOfAny(s, chars string) int {
+	return strings.LastIndexAny(s, chars)
+}
+
+// Contains 是否包含
+func Contains(s, sub string) bool {
+	return strings.Contains(s, sub)
+}
+
+// StartsWith 判断是否以某字符串打头
+func StartsWith(s, prefix string) bool {
+	return strings.HasPrefix(s, prefix)
+}
+
+// EndsWith 判断是否以某字符串打结尾
+func EndsWith(s, suffix string) bool {
+	return strings.HasSuffix(s, suffix)
+}
+
+// QuotedString 补充冒号， 通常用来做显示用
+func QuotedString(s string) string {
+	return strconv.Quote(s)
+}
+
+// DeQuotedString 去掉冒号， 通常用来做显示用
+func DeQuotedString(s string) (string, error) {
+	return strconv.Unquote(s)
+}
+
+// PadLeft 左边补齐长度
+func PadLeft(s string, totalWidth int) string {
+	strPad := ""
+	for i := 0; i < totalWidth-len(s); i++ {
+		strPad += " "
+	}
+	return strPad + s
+}
+
+// PadRight 右边补齐长度
+func PadRight(s string, totalWidth int) string {
+	strPad := ""
+	for i := 0; i < totalWidth-len(s); i++ {
+		strPad += " "
+	}
+
+	return s + strPad
+}
+
+// Substring 字符串截取
+func Substring(s string, nStartIndex, nLength int) string {
+	rs := []rune(s)
+	nRuneLen := len(rs)
+	nEndIndex := 0
+
+	// 从后往前的
+	if nStartIndex < 0 {
+		nStartIndex = nRuneLen - 1 + nStartIndex
+	}
+	nEndIndex = nStartIndex + nLength
+
+	if nStartIndex > nEndIndex {
+		nStartIndex, nEndIndex = nEndIndex, nStartIndex
+	}
+
+	if nStartIndex < 0 {
+		nStartIndex = 0
+	} else if nStartIndex > nRuneLen {
+		nStartIndex = nRuneLen
+	}
+	if nEndIndex < 0 {
+		nEndIndex = 0
+	} else if nEndIndex > nRuneLen {
+		nEndIndex = nRuneLen
+	}
+
+	return string(rs[nStartIndex:nEndIndex])
+}
+
+// Split 分割
+// usage strArr := sysutils.Split("A1,B2,C3,D4,E5,F6,G7", ",")
+func Split(s string, sep string) []string {
+	return strings.Split(s, sep)
+}
+
+// Join 字符串组合
+// usage sysutils.Join(strArr, "-") // A1-B2-C3-D4-E5-F6-G7
+func Join(a []string, sep string) string {
+	return strings.Join(a, sep)
+}
