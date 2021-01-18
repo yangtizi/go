dce1869154dffd9838b22d0bc3f02c52a825ab07
warrially <31677530+warrially@users.noreply.github.com>
2021-01-19 01:32:42 +0800

    增加断点续传代码

---
+++ download/resumebreakpoint.go
@@ -0,0 +1,94 @@
+package download
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+type TCallback func(nCurrent, nTotal int64) error
+
+// TResumeBreakPoint 断点续传
+type TResumeBreakPoint struct {
+	f        *os.File   // 文件指针
+	nCurrent int64      // 当前长度
+	nTotal   int64      // 总长度
+	cb       *TCallback // 回调
+}
+
+func NewResumeBreakPoint() *TResumeBreakPoint {
+	p := &TResumeBreakPoint{}
+	return p
+}
+
+// OpenFile 打开文件, 参考os.OpenFile
+func (m *TResumeBreakPoint) OpenFile(name string, flag int, perm os.FileMode) error {
+	f, err := os.OpenFile(name, flag, perm)
+	if err != nil {
+		return err
+	}
+
+	fstat, err := f.Stat()
+	if err != nil {
+		defer f.Close()
+		return err
+	}
+
+	m.f = f
+	m.nCurrent = fstat.Size()
+	f.Seek(m.nCurrent, 0)
+	return nil
+}
+
+// Close 关闭
+func (m *TResumeBreakPoint) Close() {
+	if m.f != nil {
+		defer m.f.Close()
+	}
+}
+
+// Download 下载文件
+func (m *TResumeBreakPoint) Download(strURL string) error {
+	req, err := http.NewRequest(http.MethodGet, strURL, nil)
+	if err != nil {
+		return err
+	}
+
+	strRange := fmt.Sprintf("bytes=%d-", m.nCurrent)
+	req.Header.Set("Range", strRange)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	defer resp.Body.Close()
+
+	io.Copy(m.f, io.TeeReader(resp.Body, m))
+
+	return nil
+}
+
+// Write 写入操作
+func (m *TResumeBreakPoint) Write(p []byte) (int, error) {
+	n := len(p)
+	m.nCurrent += int64(n)
+	if m.cb != nil {
+		return n, (*m.cb)(m.nCurrent, m.nTotal)
+	}
+	fmt.Printf("\r                                  ")
+	fmt.Printf("\r %d / %d", m.nCurrent, m.nTotal)
+	return n, nil
+}
+
+// SetCallback 设置Callback
+func (m *TResumeBreakPoint) SetCallback(cb *TCallback) {
+	m.cb = cb
+}
+
+// SetTotal 设置总数
+func (m *TResumeBreakPoint) SetTotal(nTotal int64) {
+	m.nTotal = nTotal
+}
---
+++ download/sugar.go
@@ -0,0 +1,19 @@
+package download
+
+import "os"
+
+// 下载
+
+func ResumeBreakPointDownload(strFile string, strURL string, nTotal int64, cb *TCallback) error {
+	p := NewResumeBreakPoint()
+
+	p.SetCallback(cb)
+	p.SetTotal(nTotal)
+	err := p.OpenFile(strFile, os.O_CREATE|os.O_WRONLY, 0600)
+	if err != nil {
+		return err
+	}
+
+	defer p.Close()
+	return p.Download(strURL)
+}
